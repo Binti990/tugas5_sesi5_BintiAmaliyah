0cf4dae2ea2fb16c51b30f4891fc9e347c5ea847
Binti Amaliyah <bintiamaliyah9@gmail.com>
2025-06-16 11:44:27 +0700

    tugas sesi 3

---
+++ TugasSesi3.js
@@ -0,0 +1,10 @@
+//"ini tugas sesi 3"
+let tinggi = 5;
+
+for(let i = 1; i <= tinggi; i++){
+    let baris = ' ';
+    for (let j = 1; j <=i; j++) {
+        baris += '*';
+    }
+ console.log(baris);   
+}
